051fd74d7a6ebf8bba13476e458c6b31d614ed5b
Luiz Gustavo Caciatori <luizgustavo.caciatori@gmail.com>
2020-06-22 18:36:18 -0300

    Removed the property @rna

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -1,7 +1,12 @@
 module Complement
-  @rna = {'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U'}
+  DNA_TO_RNA = {
+    'G' => 'C',
+    'C' => 'G',
+    'T' => 'A',
+    'A' => 'U'
+  }
 
   def self.of_dna(letter)
-    letter.chars.reduce(''){|rna, char| rna + @rna[char]}
+    letter.chars.reduce(''){|rna, char| rna + DNA_TO_RNA[char]}
   end
 end
