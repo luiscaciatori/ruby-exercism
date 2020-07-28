9ad2f94fba294aa3045b8faefdf700e9d8fec7e5
Luiz Gustavo Caciatori <luizgustavo.caciatori@gmail.com>
2020-07-27 22:05:10 -0300

    Created high scores example

---
+++ high-scores/high_scores.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class HighScores
+  attr_reader :scores
+
+  def initialize(scores)
+    @scores = scores
+  end
+
+  def latest
+    @scores.last
+  end
+
+  def personal_best
+    @scores.max
+  end
+
+  def personal_top_three
+    @scores
+      .sort{|a, b| b <=> a}
+      .take(3)
+  end
+
+  def latest_is_personal_best?
+    latest == personal_best
+  end
+end
